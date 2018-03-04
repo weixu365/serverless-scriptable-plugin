be46fd32ab18453ee2ca7a72e7970160476cc75b
Wei Xu <weixu365@gmail.com>
2018-03-04 22:56:39 +1100

    fix lint issues after upgrade modules

--- example/babel-then-package-to-zip/build/exec.js
+++ example/babel-then-package-to-zip/build/exec.js
@@ -1,4 +1,4 @@
-const execSync = require('child_process').execSync;
+const { execSync } = require('child_process');
 
 module.exports = {
   execSync(description, command) {
--- test/scripts/check-is-support-colors.js
+++ test/scripts/check-is-support-colors.js
@@ -1,9 +1,9 @@
 const supportsColor = require('supports-color');
 
 console.log(`check if support colors in current process? ${JSON.stringify(supportsColor)}`);
-serverless.supportColorLevel = supportsColor.level;
+serverless.supportColorLevel = supportsColor.stdout.level;
 
-if (supportsColor.level > 0) {
+if (serverless.supportColorLevel > 0) {
   console.log('Support colors in current process');
 } else {
   console.error('FAILED: DO NOT SUPPORT colors');
