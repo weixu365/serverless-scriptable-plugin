7bc76379848298c1e0d44c86f33dd5594c6a0679
Wei Xu <wxu@seek.com.au>
2022-02-01 21:37:36 +1100

    chore(test): remove colors from test script

--- test/scripts/test-with-color.js
+++ test/scripts/test-with-color.js
@@ -1,7 +1,15 @@
-const colors = require('colors');
+const colorSupport = require('color-support');
 
-const stringOne = 'This is a plain string.';
+const supportsColor = colorSupport({ ignoreTTY: true });
 
-console.log(colors.stylize(stringOne, 'blue'));
-console.log(colors.stylize(stringOne, 'green'));
-console.log(colors.stylize(stringOne, 'yellow'));
+if (!colorSupport) {
+    console.log('color is not supported')
+} else if (colorSupport.has16m) {
+    console.log('\x1b[38;2;102;194;255msupport 16m colors\x1b[0m')
+} else if (colorSupport.has256) {
+    console.log('\x1b[38;5;119msupport 256 colors\x1b[0m')
+} else if (colorSupport.hasBasic) {
+    console.log('\x1b[31msupport basic colors\x1b[0m')
+} else {
+    console.log('should not reach here, but colors are not supported')
+}
