464ac86facd798aea1cca3941ea985954c8731a8
Wei Xu <weixu365@gmail.com>
2022-06-05 10:11:30 +1000

    chore: Upgrade minimist (#137)
    Co-authored-by: Wei Xu <wxu@seek.com.au>

--- test/scripts/test-with-color.js
+++ test/scripts/test-with-color.js
@@ -2,14 +2,14 @@ const colorSupport = require('color-support');
 
 const supportsColor = colorSupport({ ignoreTTY: true });
 
-if (!colorSupport) {
-    console.log('color is not supported')
-} else if (colorSupport.has16m) {
-    console.log('\x1b[38;2;102;194;255msupport 16m colors\x1b[0m')
-} else if (colorSupport.has256) {
-    console.log('\x1b[38;5;119msupport 256 colors\x1b[0m')
-} else if (colorSupport.hasBasic) {
-    console.log('\x1b[31msupport basic colors\x1b[0m')
+if (!supportsColor) {
+  console.log('color is not supported');
+} else if (supportsColor.has16m) {
+  console.log('\x1b[38;2;102;194;255msupport 16m colors\x1b[0m');
+} else if (supportsColor.has256) {
+  console.log('\x1b[38;5;119msupport 256 colors\x1b[0m');
+} else if (supportsColor.hasBasic) {
+  console.log('\x1b[31msupport basic colors\x1b[0m');
 } else {
-    console.log('should not reach here, but colors are not supported')
+  console.log('should not reach here, but colors are not supported');
 }
